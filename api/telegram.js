3adda267b75dcee847c0fadb5d9647579cd42d4f
bhumikaliteraryaisle-sys <bhumikaliteraryaisle@gmail.com>
2026-04-06 17:42:07 +0530

    Fix response handling in Telegram bot webhook

--- api/telegram.js
+++ api/telegram.js
@@ -3,7 +3,8 @@ const bot = require('../src/bot');
 module.exports = async (req, res) => {
     try {
         if (req.method === 'POST') {
-            await bot.handleUpdate(req.body, res);
+            await bot.handleUpdate(req.body);
+            res.status(200).json({ ok: true });
         } else {
             res.status(200).send('Telegram Bot Webhook is running.');
         }
